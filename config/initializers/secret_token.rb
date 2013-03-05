d14acbcbaa1a75de5c46df7564451b1c1511ee77
Farooq Yousuf <farooqyousuf1@gmail.com>
2013-03-05 11:10:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HalalSpot::Application.config.secret_token = 'b811d9d2d914d2090bd422eb752d7c154c9be3c2b58a767ebb8f40288f4c4025c5dc34761bd525ae1ec4658987d638d3f9914fcad5ecf9c24f34cc7b5d67df61'
