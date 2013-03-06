3c6f618812d364b63569b37cc406950ba978a256
Farooq Yousuf <farooqyousuf1@gmail.com>
2013-03-05 19:43:35 -0500

    more ui changes and auth system

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 HalalSpot::Application.routes.draw do
   
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
-
-  get "static_pages/contact"
+  devise_for :users
 
   root :to => 'static_pages#home'
+  
+  match 'about' => 'static_pages#about'
+  match 'help' => 'static_pages#help'
+  match 'contact' => 'static_pages#contact'
+  
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
